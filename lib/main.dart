abc4ee3d3d9d6198e073ac4a58fe6e6222568e2a
Pranav Ajay <pranavajay2002@gmail.com>
2023-05-18 02:05:00 +0530

    Added provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:takudu_gpt/presentation/screens/chat_home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
